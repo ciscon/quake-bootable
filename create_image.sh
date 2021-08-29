7e6f6c399a7a0d31922f2d557bdf8e5ff9ffacbe
ciscon <ciscon@nicotinelounge.com>
2021-08-29 15:48:08 -0400

    support either devuan/debian

--- create_image.sh
+++ create_image.sh
@@ -7,7 +7,7 @@ quakedir="quake-base"
 imagename="quake_bootable-$(date +"%Y-%m-%d").img"
 mediahostname="quakeboot"
 
-distro="devuan" #devuan or debian
+distro="debian" #devuan or debian
 release="testing"
 
 ezquakegitrepo="https://github.com/ezQuake/ezquake-source.git"
@@ -48,6 +48,7 @@ if [ "$distro" = "devuan" ];then
 else
 	sudo debootstrap --include="debian-keyring" --exclude="devuan-keyring" --no-check-gpg --variant=minbase $release "$workdir" https://deb.debian.org/debian/
 fi
+export distro
 
 sudo mkdir -p "$workdir/etc/modprobe.d"
 sudo cp -f "$nouveauconf" "$workdir/etc/modprobe.d/nouveau.conf"
@@ -83,7 +84,7 @@ apt-get -qqy update
 apt-get -qqy install gnupg ca-certificates wget file git sudo build-essential nvidia-driver nvidia-settings xorg terminfo \
 linux-image-amd64 linux-headers-amd64 \
 firmware-linux firmware-linux-nonfree firmware-realtek firmware-iwlwifi \
-connman connman-gtk cmst iproute2 \
+connman cmst iproute2 \
 procps vim-tiny \
 feh xterm obconf openbox tint2 fbautostart menu \
 nodm \
@@ -92,20 +93,22 @@ lxrandr dex \
 alsa-utils \
 chromium 
 
-#log2ram
-echo "deb http://packages.azlux.fr/debian/ stable main" > /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | apt-key add -
-apt-get -qqy update
-apt-get -qqy install log2ram
+#log2ram on debian, devuan does not have systemd so the installation will fail
+if [ "$distro" = "debian" ];then
+	echo "deb http://packages.azlux.fr/debian/ stable main" > /etc/apt/sources.list.d/azlux.list
+	wget -qO - https://azlux.fr/repo.gpg.key | apt-key add -
+	apt-get -qqy update
+	apt-get -qqy install log2ram
+fi
 
 #configure rc.local
 chmod +x /etc/rc.local
 (systemctl enable rc-local||true)
-(rc-update add rc.local default||true)
+(update-rc.d rc.local enable||true)
 
 #nodm
 (systemctl enable nodm||true)
-(rc-update add nodm default||true)
+(update-rc.d nodm enable||true)
 
 #build ezquake
 export CFLAGS="-march=nehalem -flto=$(nproc) -fwhole-program"
@@ -114,7 +117,9 @@ rm -rf /home/quakeuser/build
 mkdir /home/quakeuser/build
 git clone --depth=1 '$ezquakegitrepo' /home/quakeuser/build/ezquake-source-official
 cd /home/quakeuser/build/ezquake-source-official
-./build-linux.sh
+eval $(grep --color=never PKGS_DEB build-linux.sh|head -n1)
+apt-get -qqy install $PKGS_DEB
+make -j$(nproc)
 strip ezquake-linux-x86_64
 cp -f /home/quakeuser/build/ezquake-source-official/ezquake-linux-x86_64 /home/quakeuser/quake/.
 git clean -qfdx
@@ -135,6 +140,7 @@ apt-get -qqy clean
 #configure /tmp as tmpfs
 sed -i "s/#RAMTMP=.*/RAMTMP=yes/g" /etc/default/tmpfs
 
+rm -rf /var/log/*
 '
 if [ $? -ne 0 ];then
 	echo "something failed, bailing out."
