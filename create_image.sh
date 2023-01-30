e1cc3292c96a7298da29c790fe07fa47d2d9d017
ciscon <ciscon@nicotinelounge.com>
2023-01-30 15:42:53 -0500

    no fast math on gcc 12

--- create_image.sh
+++ create_image.sh
@@ -162,7 +162,7 @@ if [ $onlybuild -eq 0 ] || [ ! -d "$workdir/usr" ];then
 	
 	#build ezquake
 	echo "building ezquake"
-	export CFLAGS="-march=nehalem -O3 -ffast-math -flto=$(nproc) -pipe"
+	export CFLAGS="-march=nehalem -O3 -flto=$(nproc) -pipe"
 	export LDFLAGS="$CFLAGS"
 	rm -rf /home/quakeuser/build
 	mkdir /home/quakeuser/build
