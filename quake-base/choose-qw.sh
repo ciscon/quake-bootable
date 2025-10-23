a707c540755fddaa261ee5fce0a25b24a50e55f8
ciscon <ciscon@nicotinelounge.com>
2025-10-23 16:42:39 -0400

    update qw preset script to support argument for non-interactive calls

--- quake-base/choose-qw.sh
+++ quake-base/choose-qw.sh
@@ -2,34 +2,47 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$scriptdir"
-
 current_preset=$(basename $(readlink -f qw))
 declare -a qwdirs
 
-echo "Available presets:"
+if [ -z $1 ];then
 
-count=1
-for qw in "$scriptdir/qw."* ;do
-	qwdirs[$count]=$(basename $qw)
-	if [ "${qwdirs[$count]}" = "$current_preset" ];then
-		current="(currently selected)"
-	else
-		current=""
-	fi
-	qw=${qw##*.}
-	echo "$count) $qw $current"
-	let count=count+1
-done
+        echo "Available presets:"
 
-read -p 'Choose asset preset: ' option
-if [ ${qwdirs[$option]+asdf} ];then
-	qw=${qwdirs[$option]}
-	qwclean=${qw##*.}
-	echo "Setting preset to '$qwclean'"
-	rm -f qw
-	ln -sf $qw qw
+        count=1
+        for qw in "$scriptdir/qw."* ;do
+                qwdirs[$count]=$(basename $qw)
+                if [ "${qwdirs[$count]}" = "$current_preset" ];then
+                        current="(currently selected)"
+                else
+                        current=""
+                fi
+                qw=${qw##*.}
+                echo "$count) $qw $current"
+                let count=count+1
+        done
+
+        read -p 'Choose asset preset: ' option
+        if [ ${qwdirs[$option]+asdf} ];then
+                qw=${qwdirs[$option]}
+                qwclean=${qw##*.}
+        else
+                echo "Option invalid, exiting."
+                exit 1
+        fi
 else
-	echo "Option invalid, exiting."
-	exit 1
+        qwclean=$1
+        qw=qw.${qwclean}
 fi
+
+if [ -e "$scriptdir/qw.${qwclean}" ];then
+
+        echo "Setting preset to '$qwclean'"
+        rm -f qw
+        ln -sf $qw qw
+else
+        echo "Invalid directory, exiting."
+        exit 2
+fi
+
 echo "Complete."
